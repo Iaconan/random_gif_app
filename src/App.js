4194ca327f1c807c5d89de06f92cf3952d51825d
Vincenzo Iacona <vincenzoiacona@hotmail.com>
2022-05-02 13:33:44 +0200

    random gif section

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
 import React from 'react'
+import Random from './components/Random_V1';
+import Tag from './components/Tag_V1';
+
+
 import './App.css'
 
 const App = () => {
   return (
     <div>
     <h1> Random GIF Application</h1>
+    <div className='main-container'>
+        <Random />
+        <Tag />
+    </div>
     </div>
   )
 }
